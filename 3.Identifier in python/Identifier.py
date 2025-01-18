22a1c7cf58904de4e7f36b9567e198917ddf9b26
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-01-18 19:19:54 +0530

    update

--- 3.Identifier in python/Identifier.py
+++
@@ -1,6 +0,0 @@
-x=10;
-X=50;
-_x=90;
-print(x)
-print(X)
-print(_x)
---
+++ 3.Identifier_in_python/Identifier.py
@@ -0,0 +1,10 @@
+x=10
+X=50
+_x=90
+print(x)
+print(X)
+print(_x)
+
+# * it is case sensitive language 
+# name="ajay"
+# print(Name)

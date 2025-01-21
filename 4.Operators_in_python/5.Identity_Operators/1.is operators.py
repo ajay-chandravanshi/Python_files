b9e02cfd6b2feea9c2703c48c2272210c0c7fbe7
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-01-21 18:42:37 +0530

    new code

--- 4.Operators_in_python/5.Identity_Operators/1.is operators.py
+++ 4.Operators_in_python/5.Identity_Operators/1.is operators.py
@@ -2,6 +2,11 @@ x = ["apple", "banana"]
 y = ["apple", "banana"]
 z = x
 
+print(id(x))
+print(id(y))
+
+#address  of x and y is difference in the above case 
+
 print(x is z)
 
 # returns True because z is the same object as x
@@ -13,3 +18,10 @@ print(x is y)
 print(x == y)
 
 # to demonstrate the difference betweeen "is" and "==": this comparison returns True because x is equal to y
+
+e=10
+f=10
+print(id(e))
+print(id(f))
+
+#address  of e and f is same in the above case 
---
+++ 4.Operators_in_python/6.bitwise_Operators/a nd,or,xor,not,zero fill left shift,signed right shift.py
@@ -0,0 +1,17 @@
+#and bitwise operator
+print(6&3)
+
+#OR bitwise operator
+print(6|3)
+
+#XOR bitwise operator
+print(6^3)
+
+#NOT bitwise operator
+print(~3)
+
+#Zero fill left shift bitwise operator
+print(3<<2)
+
+#Signed right shift bitwise operator
+print(8>>2)

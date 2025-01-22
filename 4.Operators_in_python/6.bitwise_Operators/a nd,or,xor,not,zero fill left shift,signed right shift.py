21ae77b9ae3be309328d586d4f2bfedb0effa5bc
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-01-22 18:41:09 +0530

    new code

--- 4.Operators_in_python/6.bitwise_Operators/a nd,or,xor,not,zero fill left shift,signed right shift.py
+++ 4.Operators_in_python/6.bitwise_Operators/a nd,or,xor,not,zero fill left shift,signed right shift.py
@@ -1,14 +1,16 @@
 #and bitwise operator
-print(6&3)
+x=10
+y=20
+print(x&y)
 
 #OR bitwise operator
-print(6|3)
+print(10|20)
 
 #XOR bitwise operator
 print(6^3)
 
 #NOT bitwise operator
-print(~3)
+print(~10)
 
 #Zero fill left shift bitwise operator
 print(3<<2)

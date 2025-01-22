db4eabb6949c6a60c97e75fed15c59a6e789a829
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-01-22 18:55:27 +0530

    new code

---
+++ 5.Literals/literals.py
@@ -0,0 +1,24 @@
+x='N'
+print(x)
+print(type(x))
+
+
+y="N"
+print(y)
+print(type(y))
+
+
+z='''N'''
+print(z)
+print(type(z))
+
+# single ('') , and double("")quotes use only in single line string
+
+u='''
+      *     
+     * *
+    * * *
+   * * * *
+  * * * * *
+        '''
+print(u)

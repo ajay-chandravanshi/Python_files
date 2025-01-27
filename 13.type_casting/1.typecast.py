88b2f7c07bb57a6594bb13a0e33de20c2e6d846f
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-01-27 19:21:24 +0530

    new code

---
+++ 13.type_casting/1.typecast.py
@@ -0,0 +1,13 @@
+
+# list to tuple
+l1=[10,20,30,40]
+print(l1)
+x=tuple(l1)
+print(x)
+print(type(x))
+
+# list to set
+y=set(l1)
+print(y)
+print(type(y))
+
---
+++ 13.type_casting/2.typecast2.py
@@ -0,0 +1,8 @@
+# string to int
+name=input("Enter your name\t")
+print(name)
+print(type(name))
+
+age=int(input("Enter your age"))
+print(age)
+print(type(age))

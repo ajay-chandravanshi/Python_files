a85ca1bb2fe6b25f56c90734dd84637f403b464a
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-01-30 19:07:15 +0530

    new code

--- 16.list methods/1.append.py
+++ 16.list methods/1.append.py
@@ -4,8 +4,16 @@ l1=[10,20,'Ajay',30,40]
 l1.append(10.5)
 print(l1)
 l1.append([10,20,30])
+print(len(l1))
 print(l1)
 
 #extend method
-l1.extend([10,20,30])
-print(l1)
+l2=[10,20,'Ajay',30,40]
+l2.extend([10,20,30])
+print(len(l2))
+print(l2)
+
+# insert method
+l3=[10,20,'Ajay',30,40]
+l3.insert(0,'Raj')
+print(l3)

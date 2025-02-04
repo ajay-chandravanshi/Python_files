05789dcfd66053ebca3fd32610627b9375cdfa1d
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-04 19:20:03 +0530

    new code

---
+++ 21.set methods/othermethods.py
@@ -0,0 +1,12 @@
+A={2,4,6,8}
+B={1,2,3,4}
+print(A.union(B))
+print(A.intersection(B))
+print(A.difference(B))
+print(A.symmetric_difference(B))
+A.intersection_update(B)
+print(A)
+B.difference_update(A)
+print(B)
+A.symmetric_difference_update(B)
+print(A)

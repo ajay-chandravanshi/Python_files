b7cc597d758f89940825d1abb6f3b50cfec41341
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-06 18:59:01 +0530

    new code

---
+++ 23.control statements/1.condition statement/if_condition.py
@@ -0,0 +1,10 @@
+n=int(input("Enter any number="))
+if(n%2==0):
+    # aage badane ke liye multiple line  select multiple line and press(tab) 
+    # piche lane ke liye multiple line select multiple line and press(shit+tab) 
+    print(f'{n} is even no.')
+    print('even no=',n)
+else:
+    print(f'{n} is odd no.')
+    print('odd no=',n)    
+print("Hello")    

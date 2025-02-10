8788df57abdc7fddc55ed7bf175ea1855fa87876
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-10 19:36:54 +0530

    new code

---
+++ 23.control statements/2.loop statement/2.for loop/print 1 to 10.py
@@ -0,0 +1,9 @@
+x=range(1,11)
+print(x)
+print(list(x))
+for i in range(1,11):
+    print(i,end=',')
+for i in range(1,11):
+    print(2*i,end=',')
+for i in range(1,11):
+    print(2*i-1,end=',')        

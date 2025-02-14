2e0bd7f7eed011f265085f33506dce671f432624
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-14 19:20:45 +0530

    new code

---
+++ 25.functions/1.declare function/sumfun.py
@@ -0,0 +1,8 @@
+def add(x,y,z):
+    "for addition of 3-variables"
+    return x+y+z
+p=int(input("Enter 1st no="))
+q=int(input("Enter 2nd no="))
+r=int(input("Enter 3rd no="))
+res=add(p,q,r)
+print(res)

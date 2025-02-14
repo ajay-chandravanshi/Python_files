1e5f0254f5ac7bd6d67fef2ed21294dbd9d1c5c9
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-14 19:26:12 +0530

    new code

--- 25.functions/1.declare function/sumfun.py
+++ 25.functions/1.declare function/sumfun.py
@@ -7,3 +7,14 @@ r=int(input("Enter 3rd no="))
 res=add(p,q,r)
 print(res)
 print(add.__doc__)
+
+# below code generate error=(TypeError: add() missing 1 required positional argument: 'z')
+# def add(x,y,z):
+#     "for addition of 3-variables"
+#     return x+y+z
+# p=int(input("Enter 1st no="))
+# q=int(input("Enter 2nd no="))
+# r=int(input("Enter 3rd no="))
+# res=add(p,q)
+# print(res)
+# print(add.__doc__)

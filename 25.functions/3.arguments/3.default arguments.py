719dfb3276f9777d249c3b4219a7278e554076b7
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-15 19:09:27 +0530

    new code

---
+++ 25.functions/3.arguments/3.default arguments.py
@@ -0,0 +1,17 @@
+
+def add(x=0,y=0,z=0):
+    "for addition of 3-variables"
+    print("X=",x)
+    print("Y=",y)
+    print("Z=",z)
+    return x+y+z
+# p=int(input("Enter 1st no="))
+# q=int(input("Enter 2nd no="))
+# r=int(input("Enter 3rd no="))
+# p=20
+# q=15
+# r=35
+res=add()
+# res=add(p)
+
+print(res)
---
+++ 25.functions/3.arguments/4.variable-length positional arguments.py
@@ -0,0 +1,7 @@
+# in this * means all 
+def add(*n):
+   print(n)
+   print(type(n))
+
+res=add(2,4,6,8,9,12,234,43,3)
+print(res)

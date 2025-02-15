120462c1ac0408611304cfe30ff012b6681c1d98
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-15 18:55:30 +0530

    new code

---
+++ 25.functions/3.arguments/2.key-word arguments.py
@@ -0,0 +1,16 @@
+
+def add(x,y,z):
+    "for addition of 3-variables"
+    print("X=",x)
+    print("Y=",y)
+    print("Z=",z)
+    return x+y+z
+# p=int(input("Enter 1st no="))
+# q=int(input("Enter 2nd no="))
+# r=int(input("Enter 3rd no="))
+p=20
+q=15
+r=35
+res=add(z=p,y=q,x=r)
+
+print(res)

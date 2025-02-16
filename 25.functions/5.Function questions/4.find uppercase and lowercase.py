25f48ea3a2c58793f4407b26941e31bd4cce239a
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-17 00:11:04 +0530

    new code

--- 25.functions/5.Function questions/4.find uppercase and lowercase.py
+++ 25.functions/5.Function questions/4.find uppercase and lowercase.py
@@ -1,10 +1,16 @@
-
+# Write a Python function that accepts a string and counts the number of upper and lower case letters.
 def findd(value):
+    UPPER_CASE=0;
+    LOWER_CASE=0;
     for i in value:
-     =i
-return value
+      if(i.isupper()):
+          UPPER_CASE= UPPER_CASE+1
+      elif(i.islower()):
+           LOWER_CASE= LOWER_CASE+1
+      else:
+          pass
+    return  UPPER_CASE,LOWER_CASE         
 
 value=eval(input("Enter the string="))
 ans=findd(value)
-print(ans)
-
+print("Uppercase and Lowercase",ans)

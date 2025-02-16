89f01a3ecd2e94d0fedb1a8ba27069592e24d46a
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-16 23:38:47 +0530

    new code

--- 25.functions/5.Function questions/2.multiply using function.py
+++ 25.functions/5.Function questions/2.multiply using function.py
@@ -1,1 +1,10 @@
 # multiply all the numbers in a list.
+def multi(x):
+    multi=1
+    for i in x:
+        multi=multi*i
+    return multi   
+
+x=eval(input("Enter the value in list="))
+ans=multi(x)
+print(ans)
---
+++ 25.functions/5.Function questions/4.find uppercase and lowercase.py
@@ -0,0 +1,10 @@
+
+def findd(value):
+    for i in value:
+     =i
+return value
+
+value=eval(input("Enter the string="))
+ans=findd(value)
+print(ans)
+

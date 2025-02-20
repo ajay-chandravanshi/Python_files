21ae671125e8cb3fb948caf14461c41c05f9ad17
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-21 03:02:26 +0530

    new code

---
+++ Practice code 2/2.While-Loop  practice questions/1. display n natural numbers..py
@@ -0,0 +1,6 @@
+#  Write a program to display n natural numbers. (In Horizontal-1,2,3,4,5…….. )
+n=int(input("Enter the natural number="))
+i=1
+while(i<=n):
+   print(i ,end=',')
+   i=i+1 
---
+++ Practice code 2/2.While-Loop  practice questions/2. calculate the sum of numbers.py
@@ -0,0 +1,8 @@
+#  Write a program to calculate the sum of numbers.
+num=int(input("Enter the Number="))
+i=1
+sum=0
+while(i<=num):
+    sum=sum+i
+    i=i+1
+print(sum)   

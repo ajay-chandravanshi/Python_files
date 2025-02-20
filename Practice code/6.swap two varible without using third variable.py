8f9fa25fa94d134e795455537d1a5f397105ddbf
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-20 14:37:17 +0530

    new code

---
+++ Practice code/6.swap two varible without using third variable.py
@@ -0,0 +1,8 @@
+x=int(input("Enter the first number="))
+y=int(input("Enter the second number="))
+print("value of x=",x,"value of y=",y)
+x=x+y
+y=x-y
+x=x-y
+print("value of x=",x)
+print("value of y=",y)

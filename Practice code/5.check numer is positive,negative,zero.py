25070dae41074f372fa7215f71b81e29dd8afa3d
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-20 14:27:36 +0530

    new code

---
+++ Practice code/5.check numer is positive,negative,zero.py
@@ -0,0 +1,7 @@
+x=int(input("Enter the number="))
+if(x>0):
+    print("Given number is positive")
+elif(x<0):
+      print("Given number is negative")  
+else:
+       print("Given number is Zero")        

b7da6ca45695b358e6fa6edfa6d0b58801a0e037
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-22 00:58:30 +0530

    new code

---
+++ Practice code 2/3.FOR_LOOP/7.count the total number of digits in a number.py
@@ -0,0 +1,9 @@
+# Python program to count the total number of digits in a number. 
+
+num=int(input("Enter the number="))
+x=str(num)
+count=0
+for i in x:
+    count=count+1
+print(count)    
+       

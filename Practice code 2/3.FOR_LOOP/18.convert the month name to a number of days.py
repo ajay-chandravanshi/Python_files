ec84f70469350598064b313eda0e3cae7be9f6f6
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-22 19:33:44 +0530

    new code

---
+++ Practice code 2/3.FOR_LOOP/18.convert the month name to a number of days.py
@@ -0,0 +1,9 @@
+# Python program to convert the month name to a number of days. 
+
+dict1={'jan':31,'feb':29}
+n='jan'
+x=dict1.keys()
+if n in x:
+    print("No. of Days=",dict[n])
+else:
+    print("Invalid Input")    

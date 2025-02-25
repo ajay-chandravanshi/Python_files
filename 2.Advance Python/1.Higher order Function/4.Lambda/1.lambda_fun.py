f28e13166f8f334414f8291d3ad4976d34862e89
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-25 18:52:15 +0530

    new code

---
+++ 2.Advance Python/1.Higher order Function/4.Lambda/1.lambda_fun.py
@@ -0,0 +1,16 @@
+# A function without name
+# use lambda keyword
+#it takes n number of argument
+# but execute only single line of expression
+
+#syntex:- Lambda arguments:expression
+
+# example:-1 write a function to add two number
+
+x=int(input("Enter the first value="))
+y=int(input("Enter the second value="))
+res=lambda p,q:p+q
+print(res(x,y))
+
+res=lambda p,q:print(p+q)
+res(x,y)

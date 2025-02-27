b69269aaf4721bf3de1d25cc1e2a1700a5526994
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-27 19:03:31 +0530

    new code

--- 2.Advance Python/1.Higher order Function/4.Lambda/3.use for loop and print.py
+++ 2.Advance Python/1.Higher order Function/4.Lambda/3.use for loop and print.py
@@ -1,10 +1,22 @@
 
-x=[[1,2,3],[1,2,3],[1,2,3]]
-l1=[]
-for i in range(1,4):
-    l1.append(i)
-print(l1)   
-l2=[]
-for i in range(4):
-    l2.append(l1)
-print(l2)     
+# x=[[1,2,3],[1,2,3],[1,2,3]]
+# l1=[]
+# for i in range(1,4):
+#     l1.append(i)
+# print(l1)   
+# l2=[]
+# for i in range(4):
+#     l2.append(l1)
+# print(l2)
+
+
+# above code solve using lambda
+
+# x='neeraj'
+# z=lambda x:[i for i in x]
+# print(z(x))
+
+# above code for loop in for loop
+
+z=lambda p:[(i for i in range(1,p)) for k in range(p)]
+print(z(4))
---
+++ 2.Advance Python/1.Higher order Function/5.Generator/generator_keyword.py
@@ -0,0 +1,14 @@
+def first():
+    # return 'Hello'
+    yield 1
+    yield 2
+    yield 3
+x=first()
+# print(x)
+# print(list(x))
+print(next(x))
+print("hi")
+print("Hello")
+print("Welcome")
+print(next(x))
+

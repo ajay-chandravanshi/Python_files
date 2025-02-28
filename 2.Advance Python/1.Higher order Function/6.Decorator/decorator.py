fe9eec668730cb8f79bd41a323631f1327e991a3
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-02-28 19:18:43 +0530

    new code

--- 2.Advance Python/1.Higher order Function/6.Decorator/decorator.py
+++ 2.Advance Python/1.Higher order Function/6.Decorator/decorator.py
@@ -9,12 +9,26 @@
 
 # decorator code 
 # code 1
-def outer_fun(new):
-    def inner_fun():
-        print("Hello")
+# def outer_fun(new):
+#     def inner_fun():
+#         print("Hello")
+#     return inner_fun
+# @outer_fun
+# def new():
+#     pass                #bus aage badane ke liye use karte hai isse error nhi aata hai
+# new()    
+
+
+def outer_fun(s):
+    def inner_fun(p,q):
+        p=p+10
+        q=q+10
+        r=s(p,q)
+        print(r)
     return inner_fun
 @outer_fun
-def new():
-    pass                #bus aage badane ke liye use karte hai isse error nhi aata hai
-new()    
-
+def new(x,y):
+    return x+y
+p=int(input("Enter the first number="))    
+q=int(input("Enter the second number="))   
+new(p,q) 

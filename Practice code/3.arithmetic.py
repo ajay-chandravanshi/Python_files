b10c05e59b6624cb12ed179f0a1ee78c9ec7ff07
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-05 23:10:24 +0530

    new code

--- Practice code/3.arithmetic.py
+++ Practice code/3.arithmetic.py
@@ -1,4 +1,4 @@
-# print("hello")
+# # print("hello")
 # print(10 + 5)
 # print(15-8)
 # print(6*3)
@@ -11,5 +11,13 @@
 # print((10-3)*2+1)
 # print(10/3)
 # print(10//3)
-print(10/5)
-print(10//5)
+# print(10/5)
+# print(10//5)
+# print(5 * 2 ** 3)
+# print(3 * (2 + 4) / 6)
+
+x=20
+y=10
+x,y=y,x
+print("Value of x",x)
+print("Value of y",y)

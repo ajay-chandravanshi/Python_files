93697b3fd3f0cf640ee0bf4aeb63211e4ac3478f
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-05 19:20:58 +0530

    update

--- 2.Advance Python/3.OOPS/variables.py
+++ 2.Advance Python/3.OOPS/variables.py
@@ -2,10 +2,28 @@
 # self ke sath jo variable hota hai ussi ko instance variable bolte hai
  
 # 1.Instance variable
+# code-1(a)
+
+# class Student:
+#     def __init__(self,name,marks):
+#         self.x=name
+#         self.y=marks
+# obj=Student('Ajay',80)  
+# print(obj.x)      
+# print(obj.y) 
+
+# above same code in another way
+#  code-1(b)
+
 class Student:
     def __init__(self,name,marks):
         self.x=name
         self.y=marks
+    def show(self): 
+        print(self.x)   
+        print(self.y)   
 obj=Student('Ajay',80)  
-print(obj.x)      
-print(obj.y)      
+obj.show()
+obj1=Student('Ajay',30)  
+obj1.show()
+

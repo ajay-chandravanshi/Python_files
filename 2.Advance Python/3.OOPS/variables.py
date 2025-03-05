7e127e4c566517f3157c1328da28c6aec0bc68fb
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-05 19:31:52 +0530

    new code

--- 2.Advance Python/3.OOPS/variables.py
+++ 2.Advance Python/3.OOPS/variables.py
@@ -15,15 +15,25 @@
 # above same code in another way
 #  code-1(b)
 
-class Student:
-    def __init__(self,name,marks):
-        self.x=name
-        self.y=marks
-    def show(self): 
-        print(self.x)   
-        print(self.y)   
-obj=Student('Ajay',80)  
-obj.show()
-obj1=Student('Ajay',30)  
-obj1.show()
+# class Student:
+#     def __init__(self,name,marks):
+#         self.x=name
+#         self.y=marks
+#     def show(self): 
+#         print(self.x)   
+#         print(self.y)   
+# obj=Student('Ajay',80)  
+# obj.show()
+# obj1=Student('Ajay',30)  
+# obj1.show()
 
+# aise variable jo obj ke uper depend nahi karta object badalne par apni value ko nahi badalta usko hum static variable ya class variable bolte hai 
+
+class Student:
+    def display(self):
+        print("Display method")
+    def show(self):
+        print( "value of x",self.x)  
+obj=Student()
+obj.x=10
+obj.show()       

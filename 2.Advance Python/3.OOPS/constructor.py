af80a87e97962ac7e754a897c3c9900ff6723946
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-05 18:50:37 +0530

    new code

--- 2.Advance Python/3.OOPS/constructor.py
+++ 2.Advance Python/3.OOPS/constructor.py
@@ -1,11 +1,12 @@
 class Student:
     def __init__(self):
-        print("This is Constructor")
-        print(id(self))
+        print("Constructor Called")
+        # print(id(self))
     def display(self):
         print("Hello")
-# obj=student 
+# obj=Student 
 # ob.display()
 obj=Student()
 # obj.display()  
-print(id(obj))         
+# print(id(obj)) 
+obj.__init__()        

6ce02e88e3da37004ae4224edb03cf2c2cbb9394
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-06 01:09:07 +0530

    new code

--- Practice code/3.arithmetic.py
+++ Practice code/3.arithmetic.py
@@ -16,8 +16,30 @@
 # print(5 * 2 ** 3)
 # print(3 * (2 + 4) / 6)
 
-x=20
-y=10
-x,y=y,x
-print("Value of x",x)
-print("Value of y",y)
+# x=20
+# y=10
+# x,y=y,x
+# print("Value of x",x)
+# print("Value of y",y)
+
+# a=8
+# b=3
+# print(a % b + a // b)
+
+# print((4 + 6) % 3)
+# print(2 ** 3 ** 2)
+# print(10 - 3 * 2 ** 2 + 5)
+
+
+# Q. Write a Python program to check if a number is even or odd without using an if statement
+
+# num = int(input("Enter a number: "))  
+# result = ["Even", "Odd"][num % 2]  
+# print("The number is", result)
+
+# print(((5 + 3) * 2) // 3 - 1)
+
+# print((4 + 6) % 3)
+
+print(100 // 6 + 100 % 6)
+

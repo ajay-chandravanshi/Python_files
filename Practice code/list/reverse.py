ec85bc06ef68763cd53d7704387b82a50ce76328
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-07 00:30:09 +0530

    new code

--- Practice code/list/reverse.py
+++ Practice code/list/reverse.py
@@ -1,4 +1,5 @@
 # 1.code-1
+# Q.1 Reverse a List:
 
 # Input: [1, 2, 3, 4, 5]
 # Output: [5, 4, 3, 2, 1]
@@ -10,20 +11,43 @@
 
 # 2.code-2
 
+# Q.2 Find the Maximum and Minimum in a List:
+
 # Input: [10, 20, 5, 8, 90]
 # Output: Max = 90, Min = 5
 
-lst = [10, 20, 5, 8, 90]
+# lst = [10, 20, 5, 8, 90]
+# # Initialize max and min with the first element of the list
+# max_value = lst[0]
+# min_value = lst[0]
+# # Loop through the list to find max and min manually
+# for num in lst:
+#     if num > max_value:
+#         max_value = num
+#     if num < min_value:
+#         min_value = num
+# print(f"Max = {max_value}, Min = {min_value}")
 
-# Initialize max and min with the first element of the list
-max_value = lst[0]
-min_value = lst[0]
+# # above same code with another type 
 
-# Loop through the list to find max and min manually
-for num in lst:
-    if num > max_value:
-        max_value = num
-    if num < min_value:
-        min_value = num
+# # Input: [10, 20, 5, 8, 90]
+# # Output: Max = 90, Min = 5
+# L1=[10,20,5,8,90]
+# y=max(L1)
+# z=min(L1)
+# print("The maximum value=",y)
+# print("The minimum value=",z)
 
-print(f"Max = {max_value}, Min = {min_value}")
+# 3.code-3
+
+# Q.3 Remove Duplicates from a List:
+
+# Input: [1, 2, 2, 3, 4, 4, 5]
+# Output: [1, 2, 3, 4, 5]
+
+L1=[1, 2, 2, 3, 4, 4, 5]
+L2=[]
+for num in L1:
+    if num not in L2:
+        L2.append(num)
+print(L2)        

65727075731aae1d14abc6628a94fc3bd7668f86
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-07 19:15:05 +0530

    new code

---
+++ 2.Advance Python/3.OOPS/method/class method.py
@@ -0,0 +1,18 @@
+# static method ko modify karne ke liye hum class method ka use karte hai
+# class method banane ke liye (@classdecorator) ka use karte hai
+
+class Book:
+    price=100
+    total_pages=500
+    def __init__(self,stu_name):
+        self.x=stu_name
+        print(self.x,Book.price,Book.total_pages)
+    @classmethod
+    def update(cls,p,q):
+        cls.price=p
+        cls.total_pages=q
+obj1=Book('Ajay')
+obj2=Book('Ayush')
+obj3=Book('Animesh')
+Book.update(150,600)
+obj4=Book('Arvind')

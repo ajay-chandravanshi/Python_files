b869ec728e1ff6deb10fd7a44af434156d816044
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-08 02:37:39 +0530

    new code

--- Practice code/list/sum of all element.py
+++ Practice code/list/sum of all element.py
@@ -34,13 +34,28 @@
 
 # Q.3 Write a Python program to count the occurrences of a specific element in a list.
 
-L1=eval(input("Enter the list element="))
-L2=list(L1)
+# L1=eval(input("Enter the list element="))
+# L2=list(L1)
+# i=0
+# count=0
+# element=int(input("Enter the element to count="))
+# while (i<len(L1)):
+#     if(L1[i]==element):
+#         count=count+1
+#     i=i+1
+# print(f"The Element {element} appears {count} times in the list")        
+
+# Q.4 Write a program to remove all duplicates from a list.
+
+L1=[10,20,10,1,3,1,4]
+L3=[]
 i=0
-count=0
-element=int(input("Enter the element to count="))
 while (i<len(L1)):
-    if(L1[i]==element):
-        count=count+1
-    i=i+1
-print(f"The Element {element} appears {count} times in the list")        
+        
+        if L1[i] not in L3:
+          L3.append(L1[i])      
+        i=i+1 
+        
+print(L3)      
+
+  

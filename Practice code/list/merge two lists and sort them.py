92a69e4e595f11c47af72c73ccf0777a6590c5ea
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-11 02:26:31 +0530

    new code

--- Practice code/list/merge two lists and sort them.py
+++ Practice code/list/merge two lists and sort them.py
@@ -1,9 +1,30 @@
 # Q.Write a Python program to merge two lists and sort them.
-L1=[10,20,1,34,2,56,3]
-L2=[11,6,3,6,10,45,32]
+
+# code -1 using bubble sort 
+
+L1=[2,20,10,1,70]
+L2=[70,2,1,90,40]
 i=0
 while(i<len(L2)):
     L1.append(L2[i])
     i=i+1
-sorted.L1
-print(L1)    
+
+n = len(L1)
+for i in range(n):
+    for j in range(n - i - 1):
+        if L1[j] > L1[j + 1]:  # Swap if current element is greater than next
+            L1[j], L1[j + 1] = L1[j + 1], L1[j]
+
+print("Sorted List:", L1)
+
+# code -2 using sort method 
+L1=[2,20,10,1,70]
+L2=[70,2,1,90,40]
+i=0
+while(i<len(L2)):
+    L1.append(L2[i])
+    i=i+1
+
+print(L1)
+L1.sort()
+print(L1)

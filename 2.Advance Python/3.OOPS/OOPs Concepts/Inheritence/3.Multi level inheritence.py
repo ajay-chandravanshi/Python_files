2ce811dd39a5891474a12028bc3319b374acf428
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-11 00:14:06 +0530

    new code

--- 2.Advance Python/3.OOPS/OOPs Concepts/Inheritence/3.Multi level inheritence.py
+++ 2.Advance Python/3.OOPS/OOPs Concepts/Inheritence/3.Multi level inheritence.py
@@ -1,7 +1,27 @@
 
+# class GrandParent:
+#     def home(self):
+#         print("from Grandparent")
+# class parent(GrandParent):
+#     def car(self):
+#         print("from parent")   
+
+# class child(parent):
+#     def car(self):
+#         print("from child")
+#         super().car()
+
+# obj=child()
+# obj.home()
+# obj.car()
+
+# code 2
+
+
 class GrandParent:
-    def home(self):
+    def car(self):
         print("from Grandparent")
+
 class parent(GrandParent):
     def car(self):
         print("from parent")   
@@ -9,8 +29,8 @@ class parent(GrandParent):
 class child(parent):
     def car(self):
         print("from child")
-        super().car()
+        super(parent,self).car()
 
 obj=child()
-obj.home()
+# obj.home()
 obj.car()
--- Practice code/list/find the second largest element in a list.py
+++ Practice code/list/find the second largest element in a list.py
@@ -1,32 +1,19 @@
 # Q. Write a program to find the second largest element in a list.
 
-# L1=[150,10,20,1,3,40,90,100,120]
-# First=L1[0]
-# Second=L1[0]
-# i=0
-# while(i<len(L1)):
-#     if(First<L1[i]):
-#         Second=First
-#         First=L1[i]
-#     elif L1[i] > Second and L1[i] != First:
-#         Second = L1[i]         
-#     i=i+1
-# print("The First Largest value=",First)        
-# print("The Second Largest value=",Second)        
+L1=[5000,100,150,10,500,1000]
+fir_larg=L1[0]
+i=1
+while(i<len(L1)):
+    if(fir_larg<L1[i]):
+        fir_larg=L1[i]
+    i=i+1
 
-L1 = [150, 10, 20, 1, 3, 40, 90, 100, 120]
+sec_larg=0
+j=0
 
-First = L1[0]
-Second = None  # Initially set Second as None
-
-i = 1  # Start loop from index 1
-while i < len(L1):
-    if L1[i] > First:
-        Second = First  # Move First to Second
-        First = L1[i]  # Update First
-    elif Second is None or (L1[i] > Second and L1[i] != First):
-        Second = L1[i]  # Update Second if it's smaller than First but greater than previous Second
-    i += 1
-
-print("The First Largest value =", First)        
-print("The Second Largest value =", Second)
+while(j<len(L1)):
+    if(sec_larg<L1[j] and L1[j]<fir_larg):
+         sec_larg=L1[j]
+    j=j+1     
+print("The First Largest Value is=",fir_larg)
+print("The Second Largest Value is=",sec_larg)

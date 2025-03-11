42f7ae7d4dbad4ab2e5237742c272d1de9aaed52
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-11 22:20:21 +0530

    new code

--- Practice code/List 2/print natural number.py
+++ Practice code/List 2/print natural number.py
@@ -1,5 +1,9 @@
+# Q.1 print natural number in the list 
+
 # L1=eval(input("Enter the natural number upto 10="))
 # print(L1)
+
+# Q.2 find the sum of the element 
 
 # L1=eval(input("Enter the list element="))
 # # L1=[10,20,30,40,50]
@@ -9,14 +13,16 @@
 #     i=i+1
 # print(sum)    
 
-L1=[100,1,34,5,1,67,56,45,90,200,0]
-i=0
-min=max=L1[i]
-while(i<len(L1)):
-    if(max<L1[i]):
-        max=L1[i]
-    elif(min>L1[i]):
-        min=L1[i] 
-    i=i+1
-print("Maximum value=",max)           
-print("Minimum value=",min)           
+# Q.3 Find the maximum and minimum in th list 
+
+# L1=[100,1,34,5,1,67,56,45,90,200,0]
+# i=0
+# min=max=L1[i]
+# while(i<len(L1)):
+#     if(max<L1[i]):
+#         max=L1[i]
+#     elif(min>L1[i]):
+#         min=L1[i] 
+#     i=i+1
+# print("Maximum value=",max)           
+# print("Minimum value=",min)      
---
+++ Practice code/list/Find the index of an element in a list.py
@@ -0,0 +1,6 @@
+# Q. Find the index of an element in a list.  
+
+L1=eval(input("Enter the element of the list="))
+element=int(input("Enter the element to find index="))
+x=L1.index(element)
+print("The index of the element is=",x)

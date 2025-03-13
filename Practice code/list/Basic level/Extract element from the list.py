21c875cf92c26c177042f3402ce3d28766bbdcc8
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-13 13:44:07 +0530

    new code

--- Practice code/list/Basic level/Extract element from the list.py
+++ Practice code/list/Basic level/Extract element from the list.py
@@ -1,11 +1,17 @@
 # Q. List Slicing: Extract [20, 30, 40] from lst = [10, 20, 30, 40, 50].
 
-lis=[10,20,30,40,50]
-extract=[]
-i=1
+# code type=1
+# lis=[10,20,30,40,50]
+# extract=[]
+# i=1
+# while(i<len(lis)-1):
+#     extract.append(lis[i])
+#     i=i+1
+# print("the extract lis is =",extract)
+# print("the remainig element in the list is=",lis[0],lis[len(lis)-1])    
 
-while(i<len(lis)-1):
-    extract.append(lis[i])
-    i=i+1
-print("the extract lis is =",extract)
-print("the remainig element in the list is=",lis[0],lis[len(lis)-1])    
+# code type=2
+
+lst = [10, 20, 30, 40, 50]
+extract_lis = lst[1:4]
+print("The extract lis is =",extract_lis)

86a03adb1e38bf853cf91deb1e2d114bcf838a7e
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-15 01:52:22 +0530

    update

--- Practice code/list/intermediate level/find prime number in a list.py
+++ Practice code/list/intermediate level/find prime number in a list.py
@@ -1,58 +1,5 @@
-# # # Q . Find all prime numbers in a list.
+# Q . Find all prime numbers in a list.
 
-# # L1=[1,4,3,5,7,2,9,11,21,23]
-# # L3=[] 
-# # for n in L1:
-# #       if(n<2):
-# #         continue
-      
-# #       count=0 
-# #       i=1
-
-# #       while(i<=n):
-# #           if(n%i==0):
-# #               count=count+1
-# #               if(count>2):
-# #                   break
-# #           i=i+1
-# #       if(count==2):
-# #           L3.append(n)
-# # print(L3)                 
-
-# # # n=7
-# # # count=0
-# # # i=1
-# # # while(i<=n):
-# # #     print(i)
-# # #     if(n%i==0):
-# # #         count=count+1
-# # #         if(count>2):
-# # #             break
-# # #     i=i+1
-# # # print("the count=",count)    
-# # # if(count==2):
-# # #     print("this is a prime number")
-# # # else:
-# # #    print("not a prime")           
-
-# L1=[10,1,3,16,7,21,23,29]
-# L3=[]
-# for n in L1:
-#     if(n<=2):
-#         continue
-#     count=0
-#     i=1
-#     while(i<n):
-#         if(n%i==0):
-#             count=count+1
-#             if(count>2):
-#               break
-#         i=i+1
-#     if(count<=2):
-#         L3.append(n)    
-# print("The prime number is=",L3)         
-# 
-# 
 
 L1=[10,1,3,16,7,21,23,29]
 L2=[]
@@ -70,3 +17,20 @@ for n in L1:
     if(count==2):
         L2.append(n)
 print(L2)               
+
+# Q . check the prime number or not 
+# n=7
+# count=0
+# i=1
+# while(i<=n):
+#     print(i)
+#     if(n%i==0):
+#         count=count+1
+#         if(count>2):
+#             break
+#     i=i+1
+# print("the count=",count)    
+# if(count==2):
+#     print("this is a prime number")
+# else:
+#    print("not a prime")           

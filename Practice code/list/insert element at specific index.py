6f7be5720657ce15032b5496d5d0a9dd77b8d6a9
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-14 16:33:59 +0530

    new code

---
+++ Practice code/list/insert element at specific index.py
@@ -0,0 +1,11 @@
+# Q . Insert 25 at index 2 in lst = [10, 20, 30, 40, 50].
+
+lst = [10, 20, 30, 40, 50]
+lst.insert(2,25)
+print(lst)
+
+# Q. Remove 30 from lst = [10, 20, 30, 40, 50].
+
+lst=[10, 20, 30, 40, 50]
+lst.remove(30)
+print(lst)

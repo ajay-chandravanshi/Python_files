26632bd6d3d8f1c79f4836dc352295c883304ce4
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-16 00:47:26 +0530

    new code

--- Practice code/list/intermediate level/find the missing number in a list.py
+++ Practice code/list/intermediate level/find the missing number in a list.py
@@ -1,5 +1,5 @@
 # Q . Find the Missing number in a list of 1 to n 
-L1=[1,3,4,6,7,9,10,11,16]
+L1=[1,3,4,6,7,9,10,11,16,24]
 i=1
 L2=[]
 n=L1[len(L1)-1]
@@ -7,4 +7,4 @@ while(i<=n):
     if i not in L1:
         L2.append(i)
     i=i+1
-print(L2)        
+print("the Missing number in a list=",L2)

930c2efe8af2fa617fe2fb796f044c066086cf53
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-16 01:51:14 +0530

    new code

--- Practice code/list/intermediate level/move all the zeros in a list to the end.py
+++ Practice code/list/intermediate level/move all the zeros in a list to the end.py
@@ -1,11 +1,13 @@
 # Q . Move all zeros in a list to the end while maintaining order
 L1=[0,10,20,0,3,4,0,5,0,7]
 L2=[]
+L3=[]
 i=0
 while(i<len(L1)):
-    if(L1[i]==0):
-        L2.append(L1[i])
-        L1.remove(L1[i])
-    i=i+1
-L1=L1+L2    
-print(L1)        
+   if(L1[i]!=0):
+      L2.append(L1[i])
+   else:
+      L3.append(L1[i])   
+   i=i+1    
+L2=L2+L3
+print(L2)        

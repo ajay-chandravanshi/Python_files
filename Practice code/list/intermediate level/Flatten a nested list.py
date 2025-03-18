764c1dea9ed9f2e256b5270ffd011449851bd787
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-19 01:10:38 +0530

    new code

--- Practice code/list/intermediate level/Flatten a nested list.py
+++ Practice code/list/intermediate level/Flatten a nested list.py
@@ -2,13 +2,16 @@
 
 # Input:  [[10, 20], [30, [40, 50]], [60]]  
 # Output: [10, 20, 30, 40, 50, 60] 
-L1=[[10, 20], [30, [40, 50]], [60]]
-L2=[]
-for i in L1:
-    for n in i:
-         L2.append(n) 
-         # print(n) 
-print(L1)
-for i in L2:
-    print(i)   
+
+L1 = [[10, 20], [30, [40, 50]], [60]]
+L2 = []
+
+while L1:
+    item = L1.pop(0)  # Remove the first element
+    if isinstance(item, list):
+        L1 = item + L1  # Flatten by adding elements back to the main list
+    else:
+        L2.append(item)  # Append non-list elements to the result
+
 print(L2)
+

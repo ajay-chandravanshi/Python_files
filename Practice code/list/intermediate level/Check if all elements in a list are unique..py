f25aeaa50f08891755eac3380447c7c3d25f58fc
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-03-23 22:43:28 +0530

    new code

---
+++ Practice code/list/intermediate level/Check if all elements in a list are unique..py
@@ -0,0 +1,22 @@
+# Q . Check if all elements in a list are unique.
+
+
+L = [1, 2, 3, 4, 5, 6]  # Example list
+i = 0
+is_unique = True
+
+while i < len(L):
+    j = i + 1
+    while j < len(L):
+        if L[i] == L[j]:  
+            is_unique = False
+            break
+        j += 1
+    if not is_unique:
+        break
+    i += 1
+
+if is_unique:
+    print("All elements are unique")
+else:
+    print("Duplicate elements found")

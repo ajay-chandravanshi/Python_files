b50b5e8fe85a2f3ac66190685ac71c46e0c92f40
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-04-04 00:25:07 +0530

    new code

---
+++ Practice codes 2/tuple/element in tuple in different data types.py
@@ -0,0 +1,6 @@
+# Q . Create a tuple with different data types and print each element using indexing.
+
+L1=eval(input("Enter the tuple="))
+for i in L1:
+    print(type(i))
+    print(i)
---
+++ Practice codes 2/tuple/find length of tuple.py
@@ -0,0 +1,4 @@
+# Q. Write a program to find the length of a given tuple.
+L1=(1,12.3,45,6,7,"ajay")
+x=len(L1)
+print(x)

d825b5b9d82e82bcfe50f12689df6e9b56bf547c
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-06-27 23:39:22 +0530

    new code

---
+++ 1.Core Python/practice file/identifierfile.py
@@ -0,0 +1,9 @@
+
+x=10
+X=200
+_x=40
+x224s=15
+name='ajay'
+print(x,X,_x,x224s)
+# print(Name) case sensitive so this is generate error
+print(name)
---
+++ 1.Core Python/practice file/mycode.py
@@ -0,0 +1,4 @@
+
+def ajay():
+    print('hello world')
+    print('this file is import to another file')
